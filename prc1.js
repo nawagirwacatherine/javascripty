5359da2d96fe4c97fe9fc6048abf46036e9e9dd8
Cathy Nawagirwa <cathynawagirwa@gmail.com>
2023-12-12 06:50:48 +0300

    added properties to object

---
+++ prc1.js
@@ -0,0 +1,17 @@
+function letterFinder(word,match){
+    for(var i=0; i<= word.length; i++){
+        if (i < word.length ){
+            console.log(word[i])
+        }
+        else if(word[i] == match)
+        {
+            console.log('Found the',match,'at',i);
+        }
+        else{
+            console.log('---No match found at',i);
+        }
+       
+    }
+
+}
+ letterFinder("test","t");
