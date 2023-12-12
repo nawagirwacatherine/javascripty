eff373a637d743b2a207c1a0bea1766e4074241b
Cathy Nawagirwa <cathynawagirwa@gmail.com>
2023-12-12 06:50:03 +0300

    done object methods

---
+++ pract.js
@@ -0,0 +1,18 @@
+var clothes =[];
+clothes.push('underpant');
+clothes.push('trouser');
+clothes.push('bra');
+clothes.push('shirt');
+clothes.push('skirt');
+clothes.pop();
+clothes.push('nicker')
+clothes.pop();
+
+console.log(clothes[2])
+
+
+var favCar = {};
+favCar.color ='yellow';
+favCar.covertible = true;
+
+console.log(favCar);
