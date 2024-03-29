0b42d0698ceb5e89ef9bded0d8fdc11ed7b43fa9
Cathy Nawagirwa <cathynawagirwa@gmail.com>
2024-03-29 13:17:54 +0300

    worked on code using switch

--- retk.js
+++ retk.js
@@ -13,3 +13,40 @@ else if (age<18){
 else {
     console.log("The value of the age variable is not numerical")
 }
+
+
+var day ="sunday";
+switch(day){
+    case"monday":
+        console.log("Making a schedule for the week")
+    
+    break;
+    case"Tuesday":
+        console.log("All workers meeting");
+    
+    break;
+    
+    case"wednesday":
+        console.log("Attending wednesday service");
+    
+    break;
+    case"Thursday":
+        console.log("going to work");
+    
+    break;
+    case"Friday":
+        console.log("gym and attending bible study");
+    
+    break;
+    case"saturday":
+        console.log("going to club");
+    
+    break;
+    case"sunday":
+        console.log("going to church");
+    
+    break;
+    default:
+        console.log("There is no such a day");
+   
+}
