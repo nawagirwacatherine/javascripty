963b85b709a3de548bcd6db8e95bf0a00f58fc8e
Cathy Nawagirwa <cathynawagirwa@gmail.com>
2024-03-30 07:02:37 +0300

    pushed items to the array

---
+++ retk3.js
@@ -0,0 +1,15 @@
+var clothes = []
+clothes.push('coat')
+clothes.push('bra')
+clothes.push('pant')
+clothes.push('trouser')
+clothes.push('legging')
+clothes.pop[5]
+clothes.push('shirt')
+console.log(clothes)
+
+
+var favCar = {}
+
+favCar.covertible = true
+console.log(favCar)
