d53826dfc7cc2e06042705c21eebe56b090afb86
Cathy Nawagirwa <cathynawagirwa@gmail.com>
2024-03-30 06:14:17 +0300

    worked with for loop

---
+++ retk2.js
@@ -0,0 +1,17 @@
+for(var i = 5;i<0; i++){
+    console.log(i)
+}
+console.log("Counting completed")
+
+
+for (var i = 5;i>=0;i--){
+    console.log(i)
+}
+console.log("countdown finished")
+
+
+var i = 5
+while( i>=0){
+    console.log(i)
+    i++
+}
